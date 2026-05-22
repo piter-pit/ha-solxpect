4bb0466dc0b0cee87e41de97e4ca8777b024d87c
piter-pit <106586114+piter-pit@users.noreply.github.com>
2026-05-22 17:24:31 +0200

    Update sensor.py

--- custom_components/solxpect/sensor.py
+++ custom_components/solxpect/sensor.py
@@ -1,36 +1,65 @@
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 DOMAIN = "solxpect"
 
-async def async_setup_entry(hass, entry, async_add_entities):
 
+async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     async_add_entities([
-        SolxpectSensor(coordinator)
+        SolxpectTodaySensor(coordinator),
+        SolxpectTomorrowSensor(coordinator),
     ])
 
 
-class SolxpectSensor(Entity):
+class SolxpectTodaySensor(CoordinatorEntity):
 
     def __init__(self, coordinator):
-        self.coordinator = coordinator
-
-    @property
-    def name(self):
-        return "SolXpect Forecast"
+        super().__init__(coordinator)
+        self._attr_name = "SolXpect Today"
+        self._attr_unique_id = "solxpect_today"
 
     @property
     def state(self):
         data = self.coordinator.data
         if not data:
-            return None
-        return round(data["forecast"][0]["wh"], 2)
+            return 0
+        return round(sum(data["today"].values()), 3)
 
     @property
     def extra_state_attributes(self):
+        data = self.coordinator.data
+        if not data:
+            return {}
         return {
-            "forecast": self.coordinator.data["forecast"]
+            "hours": data["today"]
+        }
+
+    async def async_update(self):
+        await self.coordinator.async_request_refresh()
+
+
+class SolxpectTomorrowSensor(CoordinatorEntity):
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "SolXpect Tomorrow"
+        self._attr_unique_id = "solxpect_tomorrow"
+
+    @property
+    def state(self):
+        data = self.coordinator.data
+        if not data:
+            return 0
+        return round(sum(data["tomorrow"].values()), 3)
+
+    @property
+    def extra_state_attributes(self):
+        data = self.coordinator.data
+        if not data:
+            return {}
+        return {
+            "hours": data["tomorrow"]
         }
 
     async def async_update(self):
