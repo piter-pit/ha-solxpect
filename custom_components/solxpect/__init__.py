1c53ef3b502e8c455b7a72cbac2186a62e1c7b8a
piter-pit <106586114+piter-pit@users.noreply.github.com>
2026-05-22 16:12:37 +0200

    Update __init__.py

--- custom_components/solxpect/__init__.py
+++ custom_components/solxpect/__init__.py
@@ -1,4 +1,14 @@
+from .coordinator import SolxpectCoordinator
+
 DOMAIN = "solxpect"
 
-async def async_setup(hass, config):
+async def async_setup_entry(hass, entry):
+    coordinator = SolxpectCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
