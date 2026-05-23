8798c7ec35e25abe132f46c61bd691ae93d87278
piter-pit <106586114+piter-pit@users.noreply.github.com>
2026-05-23 09:02:11 +0200

    Create const.py

---
+++ custom_components/solxpect/const.py
@@ -0,0 +1,24 @@
+DOMAIN = "solxpect"
+
+CONF_LONGITUDE = "longitude"
+CONF_LATITUDE = "latitude"
+
+CONF_CELLS_MAX_POWER = "cells_max_power"
+CONF_CELLS_AREA = "cells_area"
+CONF_CELLS_EFFICIENCY = "cells_efficiency"
+CONF_CELLS_TEMP_COEFF = "cells_temp_coeff"
+
+CONF_DIFFUSE_EFFICIENCY = "diffuse_efficiency"
+
+CONF_INVERTER_POWER_LIMIT = "inverter_power_limit"
+CONF_INVERTER_EFFICIENCY = "inverter_efficiency"
+
+CONF_AZIMUTH = "azimuth_angle"
+CONF_TILT = "tilt_angle"
+
+CONF_ALBEDO = "albedo"
+
+CONF_SHADING_ELEVATION = "shading_elevation"
+CONF_SHADING_OPACITY = "shading_opacity"
+
+CONF_IS_CENTRAL_INVERTER = "is_central_inverter"
