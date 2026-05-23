3335636a8e07151d081dd6eaa8459d9c47bc6936
piter-pit <106586114+piter-pit@users.noreply.github.com>
2026-05-23 20:49:43 +0200

    Update __init__.py

--- custom_components/solxpect/__init__.py
+++ custom_components/solxpect/__init__.py
@@ -12,3 +12,13 @@ async def async_setup_entry(hass, entry):
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
+
+async def async_unload_entry(hass, entry):
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry, ["sensor"]
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    return unload_ok
